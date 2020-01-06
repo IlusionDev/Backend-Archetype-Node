5fe586fb640f674edf51926bf22b0024cbfbfb76
Adrian Alonso Vergara <adrialo@MacBook-Pro-de-Adrian.local>
2020-01-06 23:35:30 +0100

    added passport strategies config

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,19 +1,74 @@
+import passport from "passport";
+import passportLocal from "passport-local";
+import { getRepository } from "typeorm";
+import passportJwt from "passport-jwt";
+import Bcrypt from "bcrypt";
+import User from "../entity/User";
 
-import passport from 'passport';
-import passportLocal from 'passport-local';
+const JwtStrategy = passportJwt.Strategy;
 const LocalStrategy = passportLocal.Strategy;
+const userRepository = getRepository("user");
+const ExtractJwt = passportJwt.ExtractJwt;
 
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    User.findOne({ username: username }, function (err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
-    });
-  }
-));
+interface Options {
+  jwtFromRequest: string;
+  secretOrKey: string;
+  issuer: string;
+  audience: string;
+  ignoreExpiration: boolean;
+}
+
+const opts: Options = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: "secret",
+  issuer: "accounts.examplesoft.com",
+  audience: "yoursite.net",
+  ignoreExpiration: false
+};
+
+passport.use(
+  new JwtStrategy(opts, function(jwtPayload, done) {
+    userRepository
+      .findOne(jwtPayload.sub)
+      .then((user: User) => {
+        if (user) {
+          return done(null, user);
+        } else {
+          return done(null, false);
+        }
+      })
+      .catch(error => {
+        return done(error, false);
+      });
+  })
+);
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password"
+    },
+    function(username, password, done) {
+      userRepository
+        .find({ where: [{ email: username }] })
+        .then((user: User | any) => {
+          if (!User) {
+            return done(null, false);
+          }
+
+          Bcrypt.compare(password, user.password).then(response => {
+            if (!response) {
+              return done(null, false);
+            }
+
+            return done(null, user);
+          });
+        })
+        .catch(error => {
+          return done(error, false);
+        });
+    }
+  )
+);
+export default passport;
